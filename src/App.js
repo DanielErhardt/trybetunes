94c98057a91dc5aba1cb129ff3f0bb4ae7f0aaf9
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-04-25 10:50:26 -0300

    componentes de estruturação criados + R2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -7,12 +7,59 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
+
+import Header from './components/Header';
+import Loading from './components/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      isLoading: false,
+      isUserLogged: false,
+      handleUserSubmit: this.handleUserSubmit,
+      handleInputChange: this.handleInputChange,
+    };
+  }
+
+  handleUserSubmit = async (event) => {
+    event.preventDefault();
+    const { user } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: user });
+    this.setState({
+      isLoading: false,
+      isUserLogged: true,
+    });
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+    console.log(name, value);
+  }
+
   render() {
+    const { isUserLogged } = this.state;
     return (
       <Switch>
-        <Route exact path="/" component={ Login } />
+        { isUserLogged ? (
+          <Redirect to="/search" />
+        ) : (
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Login
+                { ...this.state }
+              />
+            ) }
+          />
+        )}
+
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
   render() {
+    const MINIMUM_NAME_LENGTH = 3;
+
+    const {
+      user, isLoading,
+      handleUserSubmit, handleInputChange,
+    } = this.props;
+
+    const isButtonDisabled = user ? user.length < MINIMUM_NAME_LENGTH : true;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <form>
+            <input
+              data-testid="login-name-input"
+              type="text"
+              name="user"
+              value={ user }
+              onChange={ handleInputChange }
+            />
+            <button
+              data-testid="login-submit-button"
+              type="submit"
+              onClick={ handleUserSubmit }
+              disabled={ isButtonDisabled }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  user: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  handleUserSubmit: PropTypes.func.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+};
 
 export default Login;
