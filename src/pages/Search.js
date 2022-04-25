2b3db4043d27ab0f2ef25cad73b8929bb46a4ebb
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-04-25 11:54:31 -0300

    R5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  handleSearchInput = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
+    const MINIMUM_LENGTH = 2;
+    const { searchInput } = this.state;
+    const isButtonDisabled = searchInput ? searchInput.length < MINIMUM_LENGTH : true;
+
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        <form>
+          <input
+            data-testid="search-artist-input"
+            name="searchInput"
+            value={ searchInput }
+            onChange={ this.handleSearchInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
     );
   }
 }
